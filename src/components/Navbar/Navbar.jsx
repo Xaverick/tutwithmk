986fee8ea47f87c666c5fef8ac1480d3ec86ee3e
kartik <kartikaggarwal2004@gamil.com>
2023-07-19 15:46:33 +0530

    added toggle featutre in mobile nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,13 @@ import './Navbar.scss'
 import logo from '../../assets/logo.png'
 import { AiOutlineMenu} from 'react-icons/ai'
 import { HiX } from 'react-icons/hi'
-import { motion } from 'framer-motion'
-import {BiChevronDown} from 'react-icons/bi'
+import {BiChevronDown,BiChevronUp} from 'react-icons/bi'
+import  {motion,AnimatePresence} from "framer-motion"
+import {AiOutlineArrowDown} from 'react-icons/ai'
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false)
+  const [serviceToggle, setServiceToggle] = useState(false)
 
   return (
     <nav className='app__flex start'>
@@ -43,24 +45,50 @@ const Navbar = () => {
           <AiOutlineMenu onClick={() => setToggle(true)} />
           {toggle && (
             <motion.div 
+              className='sidebar'
               whileInView={{ x : [200,0]}} 
               transition={{duration: 0.65, ease: "easeOut"}}
 
             >
             <ul>
-              <HiX onClick={() => setToggle(false)} />
+                <HiX onClick={() => setToggle(false)} />
+                
                 <li><a href='/' onClick={() => setToggle(false)}> Home </a></li>
                 <li><a href='/about' onClick={() => setToggle(false)}> X-factor </a></li>
                 <li>
-                  <a href='/services' onClick={() => setToggle(false)}> Get Trained</a>
-                  <ul className='hover-dropdown'>
-                    <li><a href="/services/launchpad">Launchpad</a></li>
-                    <li><a href="/services/emerge">Emerge</a></li>
-                    <li><a href="/services/transcend">Transcend</a></li>
-                    <li><a href="/services/ownthatstage">Own That Stage</a></li>
-                    <li><a href="/services/legacy">Legacy</a></li>
+                  <span>
+                    <a href="/services">Get Trained</a>
+                    {serviceToggle ? <BiChevronUp onClick={() => setServiceToggle(false)} /> : <BiChevronDown onClick={() => setServiceToggle(true)} />}
+                    <AnimatePresence>
+                    {serviceToggle && (
+                    
+                      <motion.div
+                        className="answer"
+                        key="content"
+                        initial="collapsed"
+                        animate="open"                           
+                        exit={{ opacity: 0, height: 0 ,padding:0}}
+                        transition={{duration: 0.3, ease: "linear"}} 
+                        variants={{
+                        open: { opacity: 1, height: 'auto' },
+                        collapsed: { opacity: 0, height: 0 ,padding:0},
+                        
+                        }}
+                      >
+
+                          <li><a href="/services/launchpad">Launchpad</a></li>
+                          <li><a href="/services/emerge">Emerge</a></li>
+                          <li><a href="/services/transcend">Transcend</a></li>
+                          <li><a href="/services/ownthatstage">Own That Stage</a></li>
+                          <li><a href="/services/legacy">Legacy</a></li>
+                      </motion.div>
+                        
+                    )}
+                  </AnimatePresence> 
+                  </span>         
+                      
                   
-                  </ul>
+
                 
                 </li>
                 <li><a href='/contact' onClick={() => setToggle(false)}> Contact </a></li>
