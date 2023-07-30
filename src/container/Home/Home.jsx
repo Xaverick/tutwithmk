400737eae5d95006c304759abce349b5d280ff9c
kartik <kartikaggarwal2004@gmail.com>
2023-07-30 19:26:59 +0530

    made the changes as discussed on meet

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -6,6 +6,7 @@ import ReactPlayer from "react-player";
 import Testimonials from '../../components/Testimonial/Testimonials'
 import Typewriter from "typewriter-effect";
 import dots from '../../assets/dots.png'
+import quoate2dp from '../../assets/quoate2dp.png'
 
 const Home = () => {
   return (
@@ -22,28 +23,37 @@ const Home = () => {
           </p>
         </span>
 
-        <p className='desc'><span>In this World: </span> <br />It's a Revolution – It's a War. <br />
+        
+        <div className="video">
+          <iframe 
+            class="bg-video"
+            // src="https://www.youtube.com/embed/nWp5vwsuF4E?autoplay=1&rel=0&mute=1"
+            src="https://komododecks.com/embed/recordings/CSGGjCOKEASWhNy2v9YN?autoplay=1&mute=1"
+            allowFullScreen
+            allow="autoplay"
+          />
+
+        </div>
+
+        <p className='desc'><span>This World: </span> <br />A Revolution – A War. <br />
           <span style={{fontSize:"4rem"}}>Welcome to Your Clan!</span>
         </p>
 
       </div>
 
-      <div className="video">
-        <iframe 
-          class="bg-video"
-          src="https://www.youtube.com/embed/nWp5vwsuF4E?autoplay=1&rel=0&mute=1"
-          allowFullScreen
-          allow="autoplay"
-        />
-
-      </div>
 
 {/* 
       <div className='quoate-2-background'>
-        <div className='image-overlay'>   */}
+    <div className='image-overlay'>   */}
       <div className="quoate-2">
-        <h1 className='home-heading'>Here you'll get the most sophisticated & powerful weapons (Skills & Abilities) to Establish Your True Self & that's the best humanitarian deed you can ever perform on this Planet.</h1>
-        <h1 className='home-heading'>It's tough but not a suffering which you've already endured since your Awakening.</h1>
+        <div className='quoate-2-left'>
+          <h1 className='home-heading'>Here you'll get the most sophisticated & powerful weapons (Skills & Abilities) to Establish Your True Self & that's the best humanitarian deed you can ever perform on this Planet.</h1>
+          <h1 className='home-heading'>It's tough but not a suffering that you haven't already endured.</h1>
+
+        </div>
+        <div className='quoate-2-right'>
+          <img src={quoate2dp} alt="" />
+        </div>
       </div>
       {/* </div>
       </div> */}
@@ -51,16 +61,20 @@ const Home = () => {
       <div className='quoate-3'>
         <span className='heading'>
             <p><span>T R A N S F O R M A T I O N</span> <br /> IS THE GAME</p>
-            <p style={{margin:"1rem 0"}}>I Welcome You To The Unique Version of – YOU!</p>
+            <p style={{margin:"1rem 0"}}>I welcome you to your Version X</p>
         </span>
 
         <div className='four-dots'>
-          <img src={dots} alt="" />
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
         </div>
 
         <span className='heading'>
-            <p>Your Version 2.0 A Brand New, Superior, Revolutionary, Totally Revamped version of old 1.0. </p>
-            <p style={{margin:"1rem 0"}}> <br />From the past 19yrs that's exactly what I've been delivering in India & Abroad.Now, Betterment & Improvement is not what I deliver. It's good, but once again, I don't deliver anything that is "good". </p>
+            <p>Version-X: A Brand New, Superior, Revolutionary, Totally Revamped version of old you. </p>
+            <img src={dots} alt="" />
+            <p> <br />From the past 22yrs that's exactly what I've been delivering in India & Abroad. Now, Betterment & Improvement is not what I deliver... it's good, but once again, I don't deliver anything that is "Good".</p>
         </span>
       </div>
 
@@ -99,7 +113,7 @@ const Home = () => {
 
       <div className="quoate-4">
 
-        <h1 className='home-heading'>Important: "Transformation" doesn't happen with your like or interest. Transformation is The Game of Winners! It requires "I Want It" & If you have that Fuel in you then:</h1>
+        <h1 className='home-heading'>Important: <br />  <br /> "Transformation" doesn't happen with your like or interest. Transformation is The Game of Winners! It requires "I Want It" & If you have that Fuel in you then:</h1>
         <h1 className='home-heading'> <br /> What To Train? How To Train? How Hard To Train? <br /> You leave that to me. It Is My DNA! It's Your Turn Now. <br /> <br /> "Balance" is Nature's Law – I'm Ready & If You Are too then click:</h1>
       
       </div>
@@ -147,7 +161,7 @@ const Home = () => {
         <div className='owntimeline-section__left'>
             <h1 className='own'>Hail Storm - Hurricane - Volcanic Lava</h1>
 
-            <p>If You're Ready <br /> I'll turn the world upside down <br /> Including you.</p>
+            <p>If You're Ready <br /> I'll turn the world upside down - <br /> Including you.</p>
 
             <a href="/about" className='button'>
               Know More.
