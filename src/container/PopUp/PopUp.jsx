1b374ca8b35aa1c0e71d6cbccc1214318d2986fd
kartik <kartikaggarwal2004@gmail.com>
2023-08-06 15:56:45 +0530

    changed the pop up

--- src/container/PopUp/PopUp.jsx
+++ src/container/PopUp/PopUp.jsx
@@ -1,6 +1,7 @@
 import React , {useState,useEffect} from 'react'
 import './PopUp.scss'
 import coffee from '../../assets/coffee-popup.gif'
+import clickhere from '../../assets/pressButton.gif'
 
 const PopUp = ({isopen, setIsopen}) => {
   
@@ -13,7 +14,10 @@ const PopUp = ({isopen, setIsopen}) => {
                     <div className="media">
                         <img src={coffee} alt="" />
                     </div>
-                    <div className="content">
+                    <div className='content'>
+                        <img src={clickhere} alt="" onClick={() => setIsopen(false)}/>
+                    </div>
+                    {/* <div className="content">                        
                         <div className='heading'>
                             <p className='sub-heading'>Stress Buster</p>
                             <h1>K o f f e e  with  M K</h1>
@@ -22,7 +26,8 @@ const PopUp = ({isopen, setIsopen}) => {
                         <p><br />In this specially designed Stress Buster session, You can talk about Anything but I encourage you to talk about Everything. Only You & Me. No pen or paper.</p>
                         <p><br /> Therapeutic Communication! <br /> <br /> <br /> </p>
                         <a href="/services/coffewithmk" onClick={() => setIsopen(false)}>Let's Get Started</a>
-                    </div>
+                    </div> */}
+
                 </div>
             </div>
 
